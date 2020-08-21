65da9947703289eab527df41f4ba584246403c4b
abhijeet <dev.abhijeet10@gmail.com>
2020-08-21 13:59:30 +0530

    Add stars to sky

--- src/episode-1/index.js
+++ src/episode-1/index.js
@@ -18,7 +18,7 @@ context.fillStyle = "#dbbd7a";
 context.arc(width - 200, 100, 50, 0, 2 * Math.PI);
 context.fill();
 
-for (let index = 0; index < 1500; index++) {
+for (let index = 0; index < 1000; index++) {
   let x = Math.random() * width;
   let y = Math.random() * height;
   context.beginPath();
@@ -28,5 +28,15 @@ for (let index = 0; index < 1500; index++) {
     y + Math.floor(Math.random() * 1) + 10
   );
   context.strokeStyle = "darkgrey";
+  context.stroke();
+}
+
+for (let index = 0; index < 1500; index++) {
+  let x = Math.random() * width;
+  let y = Math.random() * (height / 2);
+  context.beginPath();
+  context.moveTo(x, y);
+  context.lineTo(x, y + 1);
+  context.strokeStyle = "white";
   context.stroke();
 }

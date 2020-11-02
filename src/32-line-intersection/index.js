3b8fa211d7ad487d18af83011629975497657f25
abhijeet <dev.abhijeet10@gmail.com>
2020-11-02 07:36:01 +0530

    Interim line intersection

--- src/32-line-intersection/index.js
+++ src/32-line-intersection/index.js
@@ -1,5 +1,13 @@
 /**
+ * Line = An infinite Line headed in both direction.
+ * Line segment = Line passing through 2 points.
+ * Ray = Line headed in a particular direction.
  *
+ * A line is represented by an quation y = mx + b
+ * where m is the slope and b is the y intercept
+ * y is the poin at which line crosses the y axis
+ * slope is the ratio of rate of change of y to rate of change of x coordinate
+ * m = y2-y1/x2-x1
  */
 
 import "./styles.css";
